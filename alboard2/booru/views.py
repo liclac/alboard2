cdcb9e15e1d9923f3b0dc3b9bd29fde5e58b6fd6
Johannes Ekberg <uppfinnarn@gmail.com>
2015-01-02 11:33:54 +0100

    Highlight active tab

--- alboard2/booru/views.py
+++ alboard2/booru/views.py
@@ -4,6 +4,16 @@ from .models import Pool, Post
 
 class PoolView(DetailView):
 	model = Pool
+	
+	def get_context_data(self, **kwargs):
+		context = super(DetailView, self).get_context_data(**kwargs)
+		context['active_year'] = context['object'].start_date.year
+		return context
 
 class PostView(DetailView):
 	model = Post
+	
+	def get_context_data(self, **kwargs):
+		context = super(DetailView, self).get_context_data(**kwargs)
+		context['active_year'] = context['object'].pool.start_date.year
+		return context
